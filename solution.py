1f10035167194e85ceb1585f3b2af7ef375434de
selomef <90533406+selomef@users.noreply.github.com>
2021-12-16 21:17:40 -0800

    Update solution.py

--- solution.py
+++ solution.py
@@ -47,7 +47,7 @@ def build_packet():
     # Make the header in a similar way to the ping exercise.
     
     myChecksum = 0
-    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, 1)
     myChecksum = checksum(header + data)
     
     
