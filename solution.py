82958ff540497fa7c6a9ccda67e790bc3c5fd380
selomef <90533406+selomef@users.noreply.github.com>
2021-12-16 19:39:50 -0800

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,7 +6,6 @@ import time
 import select
 import binascii
 
-
 ICMP_ECHO_REQUEST = 8
 MAX_HOPS = 30
 TIMEOUT = 2.0
@@ -40,63 +39,54 @@ def checksum(string):
 
 def build_packet():
     #Fill in start
-    myChecksum = 0
-    myID = os.getpid() & 0xFFFF
-    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, myID)
-    data = struct.pack("d", time.time())
-    myChecksum = checksum(header + data)
-    if sys.platform == 'trace':
-        myChecksum = htons(myChecksum) & 0xffff
-    else:
-        myChecksum = htons(myChecksum)
-    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, myID, 1)
-    packet = header + data
-    return packet
-
-
 
     # In the sendOnePing() method of the ICMP Ping exercise ,firstly the header of our
     # packet to be sent was made, secondly the checksum was appended to the header and
     # then finally the complete packet was sent to the destination.
 
     # Make the header in a similar way to the ping exercise.
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
+    myChecksum = checksum(header + data)
+
     # Append checksum to the header.
 
     # Don’t send the packet yet , just return the final packet in this function.
-    # Fill in end
+    #Fill in end
 
     # So the function ending should look like this
 
+    packet = header + data
+    return packet
 
 def get_route(hostname):
     timeLeft = TIMEOUT
-    tracelist1 = []  # This is your list to use when iterating through each trace
-    tracelist2 = []  # This is your list to contain all traces
+    tracelist1 = [] #This is your list to use when iterating through each trace
+    tracelist2 = [] #This is your list to contain all traces
 
-    for ttl in range(1, MAX_HOPS):
+    for ttl in range(1,MAX_HOPS):
         for tries in range(TRIES):
-            destAddr = socket.gethostbyname(hostname)
-            icmp = socket.getprotobyname("icmp")
-            mySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, icmp)
+            destAddr = gethostbyname(hostname)
 
-            # Fill in start
+            #Fill in start
             # Make a raw socket named mySocket
-            # Fill in end
+            mySocket = socket(AF_INET, SOCK_RAW, icmp)
+            #Fill in end
 
             mySocket.setsockopt(IPPROTO_IP, IP_TTL, struct.pack('I', ttl))
             mySocket.settimeout(TIMEOUT)
             try:
                 d = build_packet()
                 mySocket.sendto(d, (hostname, 0))
-                t = time.time()
+                t= time.time()
                 startedSelect = time.time()
                 whatReady = select.select([mySocket], [], [], timeLeft)
                 howLongInSelect = (time.time() - startedSelect)
-                if whatReady[0] == []:  # Timeout
+                if whatReady[0] == []: # Timeout
                     tracelist1.append("* * * Request timed out.")
-                    # Fill in start
-                    # You should add the list above to your all traces list
-                    # Fill in end
+                    #Fill in start
+                    #You should add the list above to your all traces list
+                    tracelist1.append(tracelist2)
+                    #Fill in end
                 recvPacket, addr = mySocket.recvfrom(1024)
                 timeReceived = time.time()
                 timeLeft = timeLeft - howLongInSelect
@@ -104,6 +94,7 @@ def get_route(hostname):
                     tracelist1.append("* * * Request timed out.")
                     #Fill in start
                     #You should add the list above to your all traces list
+                    tracelist1.append(tracelist2)
                     #Fill in end
             except timeout:
                 continue
@@ -111,12 +102,19 @@ def get_route(hostname):
             else:
                 #Fill in start
                 #Fetch the icmp type from the IP packet
+                icmpHeader = recvPacket[20:28]
+                request_type, code, checksum, packetID, sequence = struct.unpack("bbHHh", icmpHeader)
                 #Fill in end
                 try: #try to fetch the hostname
                     #Fill in start
+                    try:
+                        return gethostbyname(hostname)
+
+
                     #Fill in end
                 except herror:   #if the host does not provide a hostname
                     #Fill in start
+                    return none
                     #Fill in end
 
                 if types == 11:
@@ -125,25 +123,32 @@ def get_route(hostname):
                     bytes])[0]
                     #Fill in start
                     #You should add your responses to your lists here
+                    tracelist2.append(ttl, ip)
+                    return
                     #Fill in end
                 elif types == 3:
                     bytes = struct.calcsize("d")
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
+                    tracelist2.append(ttl, ip)
+                    return
                     #Fill in start
                     #You should add your responses to your lists here
+
                     #Fill in end
                 elif types == 0:
                     bytes = struct.calcsize("d")
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
+                    tracelist2.append(ttl, ip)
+                    return
                     #Fill in start
                     #You should add your responses to your lists here and return your list if your destination IP is met
                     #Fill in end
                 else:
                     #Fill in start
                     #If there is an exception/error to your if statements, you should append that to your list here
+                    print("error")
                     #Fill in end
                 break
             finally:
                 mySocket.close()
-
 
