6b44ea628eaa090d585fed01ca9669ac570c1dc7
selomef <90533406+selomef@users.noreply.github.com>
2021-12-16 21:36:27 -0800

    Update solution.py

--- solution.py
+++ solution.py
@@ -37,7 +37,7 @@ def checksum(string):
     answer = answer >> 8 | (answer << 8 & 0xff00)
     return answer
 
-def build_packet(data):
+def build_packet(data, ID):
     #Fill in start
 
     # In the sendOnePing() method of the ICMP Ping exercise ,firstly the header of our
